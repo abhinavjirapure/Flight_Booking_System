a7476651054d292be94b9087a4d73d01acffaba1
abhinav <abhinavrjirapure123.com>
2021-12-07 17:46:44 +0530

    Updated files for eureka implementation

--- flight-booking-system/src/main/java/com/flightapp/FlightBookingSystemApplication.java
+++ flight-booking-system/src/main/java/com/flightapp/FlightBookingSystemApplication.java
@@ -2,7 +2,7 @@ package com.flightapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableEurekaClient
+@EnableDiscoveryClient
 //@ComponentScan(basePackageClasses=FlightBookingCrudRepository.class)
 public class FlightBookingSystemApplication {
 
